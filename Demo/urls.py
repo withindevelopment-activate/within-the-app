cfae2171f4fd56f6e884af95e5cb2b31407848bf
SC-Workspace-apps <sarah.f@sleepy-cloud.ae>
2025-08-16 09:34:36 +0400

    lesgo

--- Demo/urls.py
+++ Demo/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'Demo'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.zid_login, name='zid_login'),
+    path('zid/login/', views.zid_login, name='zid_login'),
     path('callback/', views.zid_callback, name='zid_callback'),
     path('logout/', views.zid_logout, name='zid_logout'),
     path('zid/refresh/', views.zid_refresh_token, name='zid_refresh'),
