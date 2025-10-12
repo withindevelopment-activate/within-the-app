c5733852e09553b33d80478ed8385205d151c2bd
SC-Workspace-apps <sarah.f@sleepy-cloud.ae>
2025-10-12 12:57:13 +0400

    removed the Database_URL

--- Django_Demo/settings.py
+++ Django_Demo/settings.py
@@ -96,13 +96,13 @@ WSGI_APPLICATION = 'Django_Demo.wsgi.application'
 #     }
 # }
 
-DATABASES = {
+'''DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
-}
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
