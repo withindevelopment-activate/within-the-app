2f9aebfeb306ee94fde27771959ffd6ef3e883c2
Remaz Nameh <remaz@sleepy-cloud.ae>
2025-10-20 14:48:05 +0400

    adding user_name = "" to home

--- Demo/views.py
+++ Demo/views.py
@@ -133,6 +133,7 @@ def home(request):
     total_orders = 0
     total_products = 0
     total_revenue = 0.0
+    user_name = ""
 
     try:
         # Fetch profile information
@@ -443,7 +444,6 @@ def match_orders_with_analytics(request):
 @require_POST
 def save_tracking(request):
     try:
-        #print("ENTERED THE TRACKING FUNCTION")
 
         import json
         try:
@@ -451,7 +451,6 @@ def save_tracking(request):
         except Exception:
             return JsonResponse({"status": "error", "message": "Invalid JSON payload"}, status=400)
 
-        #print("The data received is:", data)
         if not data:
             return JsonResponse({"status": "error", "message": "No JSON payload received"}, status=400)
 
