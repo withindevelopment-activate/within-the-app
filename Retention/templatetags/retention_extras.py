41a9cdbc1d298a6c5934428bb6aad0ab42909285
Remaz Nameh <Remaz@sleepy-cloud.ae>
2026-07-16 14:04:01 +0400

    fix tag filter

--- Retention/templatetags/retention_extras.py
+++ Retention/templatetags/retention_extras.py
@@ -12,3 +12,11 @@ def replace(value, arg):
         old, new = arg.split(',', 1)
         return str(value).replace(old, new)
     return value
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    """
+    Allows accessing dictionary items with a variable key in templates.
+    Usage: {{ my_dict|get_item:my_key }}
+    """
+    return dictionary.get(key)
